f9a018efc5589e774d26314ea908f34824584acd
Howon <howonbyun@gmail.com>
2017-02-10 15:23:38 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   port: process.env.PORT || 3000,
   firebase : {
-    apiKey: "AIzaSyD6X9k5sDq87I0-N06kQ0p9kQrGzf8p0D0",
-    authDomain: "leaplock-c39f3.firebaseapp.com",
-    databaseURL: "https://leaplock-c39f3.firebaseio.com",
-    storageBucket: "leaplock-c39f3.appspot.com",
-    messagingSenderId: "491550473452"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    storageBucket: "",
+    messagingSenderId: ""
   },
   arduino: {
     port: "/dev/tty.usbmodem1421"
